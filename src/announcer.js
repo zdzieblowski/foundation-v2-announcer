cb147a456de7debe6eef341611910d9536c02de9
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2024-11-30 21:02:05 +0000

    some stuff

--- src/announcer.js
+++ src/announcer.js
@@ -17,35 +17,41 @@ async function fetchData(url) {
 
 endpoint.get('*', asyncHandler(async (req, res) => {
   console.log(req.url);
+  console.info(req.socket.remoteAddress);
 
-  if(req.url=='/EVR'){
+  let req_found = false;
+
+  if(req.url=='/EVR') {
    api = 'http://localhost:3001/api/v2/evrmore';
+   req_found = true;
   }
-  else if(req.url=='/SKYDOGE'){
-   api = 'http://localhost:3001/api/v2/skydoge';
+  else if(req.url=='/FIRO') {
+   api = 'http://localhost:3001/api/v2/firo';
+   req_found = true;
   }
 
-  console.log(`${api}`);
+  if(req_found) {
+    console.log(`${api}`);
 
-  let metadata = await fetchData(`${api}/current/metadata`);
-  let configuration = await fetchData(`${api}/current/configuration`);
-  let ports = await fetchData(`${api}/current/ports`);
-  let blocks = await fetchData(`${api}/historical/blocks`);
-  let network = await fetchData(`${api}/current/network`)
+    let metadata = await fetchData(`${api}/current/metadata`);
+    let configuration = await fetchData(`${api}/current/configuration`);
+    let ports = await fetchData(`${api}/current/ports`);
+    let blocks = await fetchData(`${api}/historical/blocks`);
+    let network = await fetchData(`${api}/current/network`)
 
-  res.send(Object.assign([{
-    'timestamp': metadata.body[0]['timestamp'],
-    'miners': metadata.body[0]['miners'],
-    'workers': metadata.body[0]['workers'],
-    'hashrate': metadata.body[0]['hashrate'],
-    'height': network.body[0]['height'],
-    'algoritm': configuration.body[0]['algorithm'],
-    'minimum_payout': configuration.body[0]['minimumPayment'],
-    'fee_percentage': configuration.body[0]['recipientFee']*100,
-    'ports': ports.body,
-    'last_block_found': blocks.body.at(-1)
-  }]));
-
+    res.send(Object.assign([{
+      'timestamp': metadata.body[0]['timestamp'],
+      'miners': metadata.body[0]['miners'],
+      'workers': metadata.body[0]['workers'],
+      'hashrate': metadata.body[0]['hashrate'],
+      'height': network.body[0]['height'],
+      'algoritm': configuration.body[0]['algorithm'],
+      'minimum_payout': configuration.body[0]['minimumPayment'],
+      'fee_percentage': configuration.body[0]['recipientFee']*100,
+      'ports': ports.body,
+      'last_block_found': blocks.body.at(-1)
+    }]));
+  }
 }));
 
 endpoint.listen(endpoint_port, () => {
